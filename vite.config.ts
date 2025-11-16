3b9a0e90c63e1f506d4d880c3fc4fefd55554138
fre0ct20 <freoct20@gmail.com>
2025-11-16 13:14:32 +0900

    ビルド時の出力先フォルダをdist→docsに変更しdistを削除

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // ビルド時の設定
+  build: {
+    outDir: 'docs' // 出力先のフォルダをdocsに設定(gitに公開するため)　デフォルトはdist
+  }
 })
