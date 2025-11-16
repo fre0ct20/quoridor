36a9c3b9726c69e01f3f6be9db5e51522eea2f31
fre0ct20 <freoct20@gmail.com>
2025-11-16 13:46:35 +0900

    build時のbaseリンクを変更

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
 
   // ビルド時の設定
+  base: 'https://fre0ct20.github.io/quoridor/docs',
+
   build: {
     outDir: 'docs' // 出力先のフォルダをdocsに設定(gitに公開するため)　デフォルトはdist
   }
