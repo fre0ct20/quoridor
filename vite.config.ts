c01e59c69f194d15f8e226cb195b8abe1daa03c4
fre0ct20 <freoct20@gmail.com>
2025-11-17 21:27:45 +0900

    baseの修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
 
   // ビルド時の設定
-  base: 'https://fre0ct20.github.io/',
+  base: 'https://fre0ct20.github.io/docs/',
 
   build: {
     outDir: 'docs' // 出力先のフォルダをdocsに設定(gitに公開するため)　デフォルトはdist
